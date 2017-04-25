b2656dcdd15eca75d6f2a741d55ed8d357124cb1
agoalofalife <agoalofalife@gmail.com>
2017-04-25 21:32:37 +0300

    add test delete action

--- tests/Unit/Actions/DeleteTest.php
+++ tests/Unit/Actions/DeleteTest.php
@@ -4,8 +4,16 @@ namespace agoalofalife\Tests\Actions;
 
 use agoalofalife\bpm\Actions\Delete;
 use agoalofalife\bpm\Contracts\Action;
+use agoalofalife\bpm\Handlers\XmlHandler;
+use agoalofalife\bpm\KernelBpm;
+use agoalofalife\Tests\KernelBpmTest;
 use agoalofalife\Tests\TestCase;
+use GuzzleHttp\Client;
+use GuzzleHttp\ClientInterface;
+use Illuminate\Config\Repository;
 use InvalidArgumentException;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\StreamInterface;
 
 class DeleteTest extends TestCase
 {
@@ -17,6 +25,10 @@ class DeleteTest extends TestCase
         $this->action = new Delete();
     }
 
+    public function test_injectionKernel()
+    {
+        $this->action->injectionKernel(new KernelBpm());
+    }
     public function test_instanceOf()
     {
         $this->assertInstanceOf(Action::class, $this->action);
@@ -42,4 +54,34 @@ class DeleteTest extends TestCase
         $this->assertEquals('?', $this->action->getUrl());
     }
 
+    public function test_processData()
+    {
+        $config = $this->mock(new Repository());
+        app()->instance('config', $config);
+
+        $kernel = $this->mock(KernelBpm::class);
+        $this->action->injectionKernel($kernel);
+
+        $curl   = $this->mock(Client::class);
+        app()->instance(ClientInterface::class, $curl);
+
+        $response  = $this->mock(ResponseInterface::class);
+        $stream    = $this->mock(StreamInterface::class);
+        $handler   = $this->mock(XmlHandler::class);
+        $kernel->shouldReceive('getCollection')->once();
+        $kernel->shouldReceive('getPrefixConfig')->once();
+        $kernel->shouldReceive('getHandler')->times(4)->andReturn($handler);
+        $handler->shouldReceive('getContentType')->once();
+        $handler->shouldReceive('getAccept')->once();
+        $handler->shouldReceive('parse')->once();
+
+        $kernel->shouldReceive('getCurl')->once()->andReturn($curl);
+        $curl->shouldReceive('request')->once()->andReturn($response);
+        $response->shouldReceive('getStatusCode')->once();
+        $response->shouldReceive('getBody')->once()->andReturn($stream);
+        $stream->shouldReceive('getContents')->once()->andReturn('');
+
+
+        $this->action->processData();
+    }
 }
