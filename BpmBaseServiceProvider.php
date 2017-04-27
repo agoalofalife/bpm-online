a76f497c8004606920c7da843ea5a65b58876614
agoalofalife <agoalofalife@gmail.com>
2017-04-27 15:56:43 +0300

    add for Laravel

---
+++ BpmBaseServiceProvider.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace agoalofalife\bpm\ServiceProviders;
+
+
+use agoalofalife\bpm\KernelBpm;
+use Illuminate\Support\ServiceProvider;
+
+class BpmBaseServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__ . '/config/apiBpm.php' => config_path('apiBpm.php')
+        ]);
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+       $this->app->bind('bpm', function(){
+           $bpm = new KernelBpm();
+           $bpm->setConfigManually('apiBpm', [
+               'UrlLogin' => config('apiBpm.UrlLogin'),
+               'Login'    => config('apiBpm.Login'),
+               'Password' => config('apiBpm.Password'),
+               'UrlHome'  => config('apiBpm.UrlHome'),
+           ]);
+           return $bpm;
+       });
+    }
+}
---
+++ config/apiBpm.php
@@ -0,0 +1,41 @@
+<?php
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | URL API Registration Bpm
+    |--------------------------------------------------------------------------
+    | URL for a cookie, for further authentication for API Bpm
+    |   https://ourSite.bpmonline.com/ServiceModel/AuthService.svc/Login
+    */
+    'UrlLogin'=>'',
+    /*
+    |--------------------------------------------------------------------------
+    | Login API Registration Bpm
+    |--------------------------------------------------------------------------
+    | Login for cookies, for further authentication for API Bpm
+    |
+    */
+
+    'Login'=>'',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password API Registration Bpm
+    |--------------------------------------------------------------------------
+    | Password for cookies, for further authentication for API Bpm
+    |
+    */
+
+    'Password'=>'',
+
+    /*
+    |--------------------------------------------------------------------------
+    | URL Our Bpm
+    |--------------------------------------------------------------------------
+    | Home Address BPM
+    |   https://ourSite.bpmonline.com/0/ServiceModel/EntityDataService.svc/
+    */
+    'UrlHome'=>'',
+
+
+];
