b0ea8f118e10446adf0f7d99c5638b5a8a81b886
agoalofalife <agoalofalife@gmail.com>
2017-04-27 16:23:05 +0300

    update composer json download https://github.com/php-mock/php-mock-mockery add test

---
+++ tests/Unit/ServiceProviders/BpmBaseServiceProviderTest.php
@@ -0,0 +1,38 @@
+<?php
+namespace agoalofalife\Tests\ServiceProviders;
+
+use agoalofalife\bpm\KernelBpm;
+use agoalofalife\bpm\ServiceProviders\BpmBaseServiceProvider;
+use agoalofalife\Tests\TestCase;
+use Illuminate\Support\ServiceProvider;
+use phpmock\mockery\PHPMockery;
+
+class BpmBaseServiceProviderTest extends TestCase
+{
+    protected $providers;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->providers = new BpmBaseServiceProvider(app());
+    }
+
+    public function test_instanceOf()
+    {
+        $this->assertInstanceOf(ServiceProvider::class, $this->providers);
+    }
+
+    public function test_register()
+    {
+        $this->providers->register();
+        $this->assertTrue(app()->bound('bpm'));
+        $this->assertInstanceOf(KernelBpm::class, app('bpm'));
+    }
+
+    public function test_boot()
+    {
+        PHPMockery::mock('agoalofalife\bpm\ServiceProviders', "config_path")->once();
+        $this->providers->boot();
+        \Mockery::close();
+    }
+}
