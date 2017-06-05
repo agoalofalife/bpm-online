269bde65cc5f8af42ac052f741d4763ae756d103
agoalofalife <agoalofalife@gmail.com>
2017-06-05 21:37:37 +0300

    add vue plugin data sync

--- panel/src/components/Panel.vue
+++ panel/src/components/Panel.vue
@@ -20,6 +20,7 @@
               <div class="control">
                 <div class="select is-fullwidth col-md-4">
                   <select class="form-control">
+                    <option  value="" disabled selected>Select one of the options</option>
                     <option  v-for="date in dateFilter">{{ date }}</option>
                   </select>
                 </div>
@@ -51,13 +52,6 @@
                 dateFilter : [],
                 error : false,
                 notRequest:'(Unfortunately you have no requests ...)',
-                params: {
-                    symbol: 'AAPL',
-                    numberOfDays: 450,
-                    dataPeriod: 'Month'
-                },
-                symbols: ['AAPL', 'MSFT', 'JNJ', 'GOOG'],
-                periods: ['Day', 'Week', 'Month', 'Quarter', 'Year'],
                 data: [],
                 labels: [],
                 isloading: false,
@@ -74,6 +68,22 @@
                     }
                 }
             }
+        },
+        asyncData() {
+            console.log( '?A' );
+            return this.$http({
+                url: '/api/listDates',
+            }).then((response) => {
+                this.error = false
+                console.log( 'response' );
+                return {
+                    dateFilter : JSON.parse(response.data).date
+                }
+
+//                this.dateFilter = JSON.parse(response.data).date
+            }).catch((error) => {
+                this.error = true
+            })
         },
         computed: {
             stockData () {
@@ -109,14 +119,14 @@
                     this.error = true
                 })
 
-                  this.$http({
-                    url: '/api/listDates',
-                }).then((response) => {
-                    this.dateFilter = JSON.parse(response.data).date
-                    this.error = false
-                }).catch((error) => {
-                    this.error = true
-                })
+//                  this.$http({
+//                    url: '/api/listDates',
+//                }).then((response) => {
+//                    this.dateFilter = JSON.parse(response.data).date
+//                    this.error = false
+//                }).catch((error) => {
+//                    this.error = true
+//                })
             }
         }
     }
--- panel/src/main.js
+++ panel/src/main.js
@@ -4,8 +4,10 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import resource from 'vue-resource'
-Vue.use(resource)
+import VueAsyncData  from 'vue-async-data'
 
+Vue.use(resource)
+Vue.use(VueAsyncData)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
